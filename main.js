70d05677a28e35c91a303cf485ef9ab84848cf85
Santaigo Leon <monsalveleonsantiago@gmail.com>
2025-01-28 16:58:56 -0500

    Agregue la funcionalidad de que podemos aumentar la cantidad y dismnuirla para que el precio se vaya aaumentando o dismonuyendo

--- main.js
+++ main.js
@@ -1,2 +1,47 @@
-const myCarrusel = document.getElementById("carouselExampleCaptions");
-const carrusel1 = new boots
+
+const productos = document.querySelectorAll(".card");
+
+
+
+productos.forEach((producto) => {
+  const precioUnitario = producto.querySelector("#precioUnitario");
+  const cantidadDeElemento = producto.querySelector("#cantidad");
+  const precioTotal = producto.querySelector("#precioTotal");
+  const botonIncremento = producto.querySelector("#incrementar");
+  const botonDecremento = producto.querySelector("#decrementar");
+
+  let precioUnitari = parseFloat(precioUnitario.textContent);
+  let cantidad = parseInt(cantidadDeElemento.textContent);
+
+  function actualizarPrecioTotal(){
+    const total = cantidad * precioUnitari;
+    precioTotal.textContent = total.toFixed(3);
+  }
+  
+  function incrementar(){
+    cantidad++;
+    cantidadDeElemento.textContent = cantidad;
+    actualizarPrecioTotal()
+    botonDecremento.disabled = false;
+  }
+
+  function decrementar(){
+    if(cantidad > 1){
+      cantidad--;
+      cantidadDeElemento.textContent = cantidad;
+      actualizarPrecioTotal()
+    } 
+
+    if(cantidad === 1){
+      botonDecremento.disabled = true;
+    }
+  }
+
+  botonIncremento.addEventListener("click",incrementar);
+  botonDecremento.addEventListener("click",decrementar);
+
+  if(cantidad === 1 ){
+    botonDecremento.disabled = true;
+  }
+
+});
